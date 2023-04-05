f7ee4d291df6fa4a3866e8e3fbd3be145320ad8f
GABRIELalmeida0 <gabrielsantos10112002@gmail.com>
2023-04-05 15:14:38 -0300

    Adicionando ao script, interação com variáveis de timpos diferentes, resultando em comportamentos de saída diferentes

---
+++ 02_meu _primeiro_script.py
@@ -0,0 +1,5 @@
+#Importando um módulo 
+import sys #Utilizat este módulo, para conseguir ler a versão do sistema operacional.
+
+so = sys.platform
+print(f"A plataforma do S.O que você está usando é {so}") #Impressão do nome do s.o.
