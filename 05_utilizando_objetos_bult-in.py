b75e4ca571c743bc64b411308fe0e550db7dd37d
Gabriel <107738969+GABRIELalmeida0@users.noreply.github.com>
2023-04-07 19:01:53 -0300

    Atividade concluida ( :

---
+++ 05_utilizando_objetos_bult-in.py
@@ -0,0 +1,22 @@
+S = "spam"
+#print(len(S)) #mostrar o tamanho de s
+#print(S[0]) #mostrando o primeiro ínidice de posição 0
+#print(S[1:3]) #definindo um intervalo que começa na posição 1 e vai até a posição 2
+#print(S + "xyz") #concatençaõ de SPAM com XYZ
+#print(S.find("pa")) #Retorna a posição da string que está sendo passada
+#print(S.upper()) #Retorna a string em maiúsculo
+#print(S.isalpha()) #Retorna verdadeiro se oque foi passado estiver dentro do alfabeto a-z
+#print(S.replace("pa","ca")) #retorna uma nova string com os caracteres substituidos por um determinado caractere
+
+L = [123, "casa", 3.14]
+#len(L) #Retorna o tamanho da lista 
+#L[0] #Retorna aposição 0 da lista
+#print(L * 2) #Cria uma nova lista com os itens duplicados
+#L.append("bola") #Acrescenta "bola" ao final da lista 
+
+#M = [[1,2,3],[4,5,6],[7,8,9]]
+#col2 = [n[1] for n in M]#para cada n(item(esses itens são outras listas)) na lista M, pegue o segundo item
+D = {"com":"ABC", "num":23}
+#D["num"] #Pega o valor da chave "num" que é 23
+#D["cor"] = "azul" #Adiciona chave e valorao final da lista
+
